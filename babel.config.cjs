5b7115da479cbedd51a53dc93f3da3a08251a9b3
oyinda-subair <oyinda.subair@gmail.com>
2023-04-05 00:31:07 +0100

    Jest config setup

---
+++ babel.config.cjs
@@ -0,0 +1,17 @@
+module.exports = function (api) {
+  api.cache(true);
+  const presets = [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+  ];
+
+  return {
+    presets,
+    plugins: [
+      '@babel/plugin-transform-runtime',
+      'babel-plugin-transform-import-meta',
+      'babel-plugin-transform-vite-meta-env',
+    ],
+  };
+};
